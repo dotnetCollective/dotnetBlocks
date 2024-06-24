7f3aaa37062dea6bcd6294c6540e5813e453ba72
Brian Rogers <Brian@austin-rogers.com>
2024-06-24 14:47:11 -0400

    Re-structured and adding some token functionality.

---
+++ tests/dotNetBlocks.System.IO.Tests/CancellationTokenExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace CancellationTokenExtensionsTests
+{
+
+	[TestClass]
+	public class CancellationTokenExtensionsTests
+	{
+		[TestMethod]
+		public void does_dispose_cancel_the_token()
+		{
+			using (var cts = new CancellationTokenSource())
+			{
+				var token = cts.Token;
+			}
+		}
+	}
+}
